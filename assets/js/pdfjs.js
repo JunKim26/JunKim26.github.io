dfb0427e2a1ccacc5099e2e98a81f0772d261430
JunKim26 <54968347+JunKim26@users.noreply.github.com>
2021-08-02 21:41:57 -0700

    Update pdfjs.js

--- assets/js/pdfjs.js
+++ assets/js/pdfjs.js
@@ -1,7 +1,7 @@
 
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = 'https://junkim26.github.io/assets/pdf/mobile_prototypes.pdf';
+var url = 'https://junkim26.github.io/assets/pdf/prototypes-m.pdf';
 //https://f.hubspotusercontent10.net/hubfs/5957063/TRG_E_Books/trg_retirement_guides/TRG-general-retirement-guide.pdf
 
 
